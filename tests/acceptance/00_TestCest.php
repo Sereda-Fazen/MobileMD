7a4c68e7e4b0dc4358afa42fdb897cc9d4d0d123
Sereda-Fazen <fazen7@mail.ru>
2016-06-14 18:45:39 +0300

    mobileVersion filter-15

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -9,38 +9,9 @@ class TestCest
 
 
 
-    function mobilePurchaseMultipleDifferentProductsSameSupplie(\Step\Acceptance\CheckoutSteps $I,\Page\Checkout $checkoutPage){
-        $I->mobileSelectBrand('Flymo grass trimmer','Flymo Vacs & Blowers','2');
-       // $checkoutPage->checkOrder('mowdirect@gmail.com','123456');
-    }
-
-    /**
-     * @param Acceptance\CheckoutSteps $I
-     * T923_Purchase Multiple Number of Products
-     * @param \Page\Checkout $checkoutPage
-     */
-
-
-
-    function mobilePurchaseMultipleNumberProducts(\Step\Acceptance\CheckoutSteps $I,\Page\Checkout $checkoutPage){
-        $I->mobileSelectBrands();
+    function mobileTractorSale(Step\Acceptance\CheckoutSteps $I, \Page\Checkout $checkoutPage) {
+        $I->mobileSellTractor();
         $checkoutPage->checkOrder('mowdirect@gmail.com','123456');
-    }
-
-
-    /**
-     * @param \Page\Search $search
-     * T921_Purchase Tractor with custom option
-     * @param \Page\Checkout $checkoutPage
-     * @internal param Acceptance\LoginSteps $I
-     */
-
-
-    function purchaseTractorOptional(\Page\Search $search, \Page\Checkout $checkoutPage) {
-        $search->search();
-        $search->searchWrong('Lawnflite accessories optional');
-        $search->purchaseTractorOptional();
-        //$checkoutPage->checkOrder('mowdirect@gmail.com','123456');
     }
 
 
