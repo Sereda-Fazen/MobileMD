e7fb0de21c634e4b9c7ffa7664e584b3d2eace5c
Sereda-Fazen <fazen7@mail.ru>
2016-06-15 11:35:14 +0300

    mobileVersion filter-21

--- tests/acceptance/01_MobileSearchCest.php
+++ tests/acceptance/01_MobileSearchCest.php
@@ -7,20 +7,34 @@ use \Step\Acceptance;
 class SearchCest
 {
 
-    function checkSearchInvalid(Page\Search $search, \AcceptanceTester $I) {
+    function T985SearchTops10(Page\Search $search, \AcceptanceTester $I) {
+        $search->search();
+        $search->searchWrong('Top10');
+        $I->seeElement('//a[@class="gs-title"]/b[text()="Top 10"]');
+
+    }
+
+    function T986SearchNoResult(Page\Search $search, \AcceptanceTester $I) {
         $search->search();
         $search->searchWrong('invalid');
         $I->see('No Results', '//div[@class="gs-snippet"]');
     }
 
-    function checkSearchNumber(Page\Search $search, \AcceptanceTester $I) {
+    function T987SearchPlural(Page\Search $search, \AcceptanceTester $I) {
+        $search->search();
+        $search->searchWrong('Lawnflite Mini Rider 60RDE Ride-On Mower');
+        $I->see('Lawnflite Mini Rider 60RDE Ride', '//div[@class="gsc-results gsc-webResult"]/div//b');
+
+    }
+
+    function T988SearchMisspelling(Page\Search $search, \AcceptanceTester $I) {
         $search->search();
         $search->searchWrong('Lawn7Garden');
         $I->see('Showing results for Lawn 7 Garden', '//div[@class="gsc-webResult gsc-result"]');
         $I->see('Search instead for Lawn7Garden', '//div[@class="gsc-webResult gsc-result"]');
     }
 
-    function checkSearchSuccess(Page\Search $search, \AcceptanceTester $I) {
+    function T989SearchFewResult(Page\Search $search, \AcceptanceTester $I) {
         $search->searchInvalid('Lawn Garden Tractors');
         $I->seeElement('//div[@class="gsc-result-info"]');
         $I->see('Lawn Garden Tractors','//div[@class="std"]');
