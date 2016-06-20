8a43e8477de3b8529d57b92d7e01005fb198b8e4
Sereda-Fazen <fazen7@mail.ru>
2016-06-20 16:17:29 +0300

    mobileVersion filter-29

--- tests/_support/Step/Acceptance/CheckoutSteps.php
+++ tests/_support/Step/Acceptance/CheckoutSteps.php
@@ -64,6 +64,7 @@ class CheckoutSteps extends \AcceptanceTester
         $I->waitForElement('//*[@id="menu-mobile-1935"]/div/a/span');
         $I->click('//*[@id="menu-mobile-1935"]/div/a/span');
         $I->waitForElement('//p[@class="action"]/button');
+        $I->wait(2);
         $I->click('//p[@class="action"]/button');
         $I->see('was added to your shopping cart.','//li[@class="success-msg"]');
 
